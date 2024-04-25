903711d957fd9ac73b0c522f2d98d8fc6cf7edd8
whitestorm2346 <andrew770426@gmail.com>
2024-04-26 02:54:21 +0800

    Still need to figure out how to concur the problem of Bluetooth advertisement...

--- app/src/main/java/com/example/bluetoothgate/BLEBeaconAdvertise.kt
+++ app/src/main/java/com/example/bluetoothgate/BLEBeaconAdvertise.kt
@@ -9,12 +9,15 @@ import android.bluetooth.le.BluetoothLeAdvertiser
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.bluetoothgate.databinding.ActivityBleBeaconAdvertiseBinding
 
 class BLEBeaconAdvertise : AppCompatActivity() {
+    private val TAG = "BLEBeaconAdvertise"
+    private val REQUEST_BLUETOOTH_PERMISSION = 123
+
     private lateinit var binding: ActivityBleBeaconAdvertiseBinding
     private lateinit var qrCodeResult: String
 
@@ -34,19 +37,17 @@ class BLEBeaconAdvertise : AppCompatActivity() {
 
         qrCodeResult = intent.getStringExtra("qrCodeResult").toString()
 
-        binding.textView.text = qrCodeResult
-
         bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothAdapter = bluetoothManager.adapter
         bluetoothLeAdvertiser = bluetoothAdapter.bluetoothLeAdvertiser
-
-        val qrCodeBytes = qrCodeResult.toByteArray(Charsets.UTF_8) // 將 QR 碼數據轉換為字節碼
 
         val settings = AdvertiseSettings.Builder()
             .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
             .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
             .setConnectable(false) // 不允許連接
             .build()
+
+        val qrCodeBytes = qrCodeResult.toByteArray(Charsets.UTF_8)
 
         val data = AdvertiseData.Builder()
             .addManufacturerData(0, qrCodeBytes) // 添加 QR 碼數據
@@ -57,17 +58,33 @@ class BLEBeaconAdvertise : AppCompatActivity() {
                 Manifest.permission.BLUETOOTH_ADVERTISE
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Toast.makeText(this, "Bluetooth Advertise not support", Toast.LENGTH_SHORT).show()
-            return 
+            Log.d(TAG, "bluetooth advertise permission is not granted")
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.BLUETOOTH_ADVERTISE),
+                REQUEST_BLUETOOTH_PERMISSION
+            )
         }
+
+        binding.textView.setText(R.string.beacon_advertising)
+        binding.imageView.setImageResource(R.drawable.baseline_bluetooth_audio_24)
         bluetoothLeAdvertiser.startAdvertising(settings, data, null)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_BLUETOOTH_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 使用者已授予藍芽權限，可以開始執行藍芽操作
+            } else {
+                // 使用者拒絕了藍芽權限，您可以處理相應的情況
+            }
+        }
     }
 
     override fun onDestroy() {
