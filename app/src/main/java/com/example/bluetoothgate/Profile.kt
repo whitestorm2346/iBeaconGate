af0731109b325f5f646e33845c521c9ec6f358fc
whitestorm2346 <andrew770426@gmail.com>
2024-05-05 04:29:27 +0800

    Connected to Firestore for profile data

--- app/src/main/java/com/example/bluetoothgate/Profile.kt
+++ app/src/main/java/com/example/bluetoothgate/Profile.kt
@@ -5,23 +5,71 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.bluetoothgate.databinding.FragmentProfileBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class Profile : Fragment() {
     private lateinit var binding: FragmentProfileBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Profile on create")
-    }
+    private lateinit var database: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        Log.d(TAG, "Returning the view")
         return binding.root
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Profile on create")
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        Log.d(TAG, "Finished binding")
+
+        binding.entryName.setText("測試名")
+        binding.entryGender.setText("男")
+        binding.entryId.setText("A123456789")
+        binding.entryPhone.setText("0123456789")
+        binding.entryReason.setText("這個專案快失火了")
+
+        database = FirebaseFirestore.getInstance()
+        Log.d(TAG, "Firestore Instance: $database")
+
+        binding.storeButton.setOnClickListener {
+            Log.d(TAG, "storeButton on click")
+
+            val name = binding.entryName.text.toString()
+            val gender = binding.entryGender.text.toString()
+            val id = binding.entryId.text.toString().uppercase()
+            val phone = binding.entryPhone.text.toString()
+            val reason = binding.entryReason.text.toString()
+
+            val currentDateTime = LocalDateTime.now()
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            val formattedDateTime = currentDateTime.format(formatter)
+
+            val user = hashMapOf(
+                "name" to name,
+                "gender" to gender,
+                "id" to id,
+                "phone" to phone,
+                "reason" to reason,
+                "datetime" to formattedDateTime,
+                "uuid" to null
+            )
+            Log.d(TAG, "User Data: $user")
+
+            database.collection("Users").document(id).set(user)
+                .addOnSuccessListener {
+                    Toast.makeText(requireContext(), "successfully stored the data", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(requireContext(), "Failed storing the data", Toast.LENGTH_SHORT).show()
+                }
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/example/bluetoothgate/User.kt
@@ -0,0 +1,12 @@
+package com.example.bluetoothgate
+
+data class User(
+    val name: String? = null,
+    val gender: String? = null,
+    val id: String? = null,
+    val phone: String? = null,
+    val reason: String? = null,
+
+    val uuid: String? = null,
+    val dateTime: String? = null
+)
